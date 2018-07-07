85d7a34d02cf1d9efba5ee06013745e850202c1d
Makoto Mizukami <comm@makotom.org>
2018-07-07 18:27:12 +0900

    More appropriate path definition

--- client.js
+++ client.js
@@ -1,5 +1,5 @@
 addEventListener('DOMContentLoaded', () => {
-    const wsHost = 'ws://:8080/';
+    const wsHost = `ws://${location.host}/`;
     const ws = new WebSocket(wsHost);
     
     const overlay = document.querySelector('div#overlay');
